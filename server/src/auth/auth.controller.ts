80cfa31e283f71f8d5210ee24b37b6b6bac944c3
BohdanSereda <zxysereda.bodik@gmail.com>
2022-10-19 00:02:04 +0300

    update auth controller

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
-  @Get()
-  auth() {
+
+  @Post('/login')
+  async registration(@Body() userDto: CreateUserDto) {
+    return await this.authService.registration(userDto)
+  }
+
+  @Post('/registration')
+  login(@Body() userDto: CreateUserDto) {
+    return this.authService.login(userDto);
   }
 }
