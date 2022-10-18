afbcf420706d0d22b544c768dc9f1d3b811f0794
BohdanSereda <zxysereda.bodik@gmail.com>
2022-10-19 00:01:44 +0300

    formatted

--- server/src/main.firebase-functions.ts
+++ server/src/main.firebase-functions.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin'
+
 const serviceAccount = require('../../serviceAccountKey.json')
 
 const server = express();
@@ -13,14 +14,12 @@ export const createNestServer = async (expressInstance) => {
         AppModule,
         new ExpressAdapter(expressInstance),
     );
-
     return app.init();
 };
 
 createNestServer(server)
     .then((v) => console.log('Nest Ready'))
     .catch((err) => console.error('Nest broken', err));
-
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 })
